f248227260c7d798b22b1a21a568c35a2017d1ee
cowglow <cowglow@gmail.com>
2019-02-27 23:23:20 +0100

    Getting started with notation

--- code/chapter07/snapterest/source/app.js
+++ code/chapter07/snapterest/source/app.js
@@ -1,6 +1,11 @@
+/** Snapterest App from React Essentials by Artemij Fedosejev */
+
+/** Imports */
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+/** Bootstrap the app */
 var Application = require('./components/Application.react');
 
+/** Render the app */
 ReactDOM.render(<Application />, document.getElementById('react-application'));
